c1de23942ab7398a9166ada03b4582a41f150c46
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-11-29 15:14:50 +0500

    remove todo

--- core/src/main/java/xyz/bobkinn/indigoi18n/template/ArgConverters.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/template/ArgConverters.java
@@ -326,6 +326,5 @@ public class ArgConverters {
     }
 
     // TODO should we move alignment into post-converter step instead of inside for each.
-    // TODO also should reprs always format arg as string (or generic type in future) and then pass it string converter?
 
 }
