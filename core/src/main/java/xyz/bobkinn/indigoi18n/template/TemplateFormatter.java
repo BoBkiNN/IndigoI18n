4d7c627ec852a4e4beed183f937396b02fb0c573
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-11-29 17:22:07 +0500

    update tests and make correct reprs

--- core/src/main/java/xyz/bobkinn/indigoi18n/template/TemplateFormatter.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/template/TemplateFormatter.java
@@ -13,13 +13,38 @@ public abstract class TemplateFormatter<O> {
 
     public TemplateFormatter() {
         registerDefaultConverters();
+        registerDefaultReprCreators();
     }
 
     protected abstract void registerDefaultConverters();
 
     @SuppressWarnings("unchecked")
+    public <T> void registerRepr(Class<T> cls, Function<T, O> f) {
+        reprCreators.put(cls, (Function<Object, O>) f);
+    }
+
+    protected void registerDefaultReprCreators() {
+        // represent number as-is
+        registerRepr(Number.class, n -> createText(n.toString()));
+        // anything other will be quoted as default behaviour
+    }
+
+    @SuppressWarnings("unchecked")
     public <T> ArgumentConverter<T, O> getConverter(T value) {
         return (ArgumentConverter<T, O>) converters.get(value.getClass());
+    }
+
+    protected Function<Object, O> resolveReprCreator(Class<?> cls) {
+        // exact match
+        var f = reprCreators.get(cls);
+        if (f != null) return f;
+        // by base class
+        for (var e : reprCreators.entrySet()) {
+            if (e.getKey().isAssignableFrom(cls)) {
+                return e.getValue();
+            }
+        }
+        return null;
     }
 
     public static String stringRepr(String value) {
@@ -34,7 +59,7 @@ public abstract class TemplateFormatter<O> {
     public O createRepr(Object object) {
         if (object == null) return createText(stringRepr("null"));
         var cls = object.getClass();
-        var creator = reprCreators.get(cls);
+        var creator = resolveReprCreator(cls);
         if (creator != null) return creator.apply(object);
         return createText(stringRepr(String.valueOf(object)));
     }
