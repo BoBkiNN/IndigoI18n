d67201d50156b4f8ac05c8f8a0ab5cc93e08cfb4
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-08 13:23:05 +0500

    add doc

--- core/src/main/java/xyz/bobkinn/indigoi18n/StringI18n.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/StringI18n.java
@@ -4,6 +4,10 @@ import xyz.bobkinn.indigoi18n.format.impl.StringI18nFormat;
 import xyz.bobkinn.indigoi18n.format.impl.StringI18nMixin;
 
 
+/**
+ * Simple I18n class. It uses only {@link StringI18nFormat} formatter and implements {@link StringI18nMixin}.
+ * @see StringI18nFormat
+ */
 public class StringI18n extends I18n implements StringI18nMixin {
 
     @Override
