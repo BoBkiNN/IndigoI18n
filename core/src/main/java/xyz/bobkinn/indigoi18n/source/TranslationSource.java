eccc2172aaeb1ebaa7d0d4c6b93b3f7522862c76
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-09 19:08:43 +0500

    add docs to TranslationSource

--- core/src/main/java/xyz/bobkinn/indigoi18n/source/TranslationSource.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/source/TranslationSource.java
@@ -6,7 +6,15 @@ import java.net.URI;
 
 public interface TranslationSource {
 
+    /**
+     * Called by {@link xyz.bobkinn.indigoi18n.I18n#load(TranslationSource)}
+     * to add translations from source into translations table
+     * @param to text adder that is used to add translations
+     */
     void load(ISourceTextAdder to);
 
+    /**
+     * @return URI displayed in debug to distinguish between different sources
+     */
     @Nullable URI getLocation();
 }
