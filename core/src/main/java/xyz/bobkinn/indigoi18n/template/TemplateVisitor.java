3ba765a7805c8eac07b26dfff8b62c1c44954c6e
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-11 11:23:11 +0500

    add TemplateVisitor class

---
+++ core/src/main/java/xyz/bobkinn/indigoi18n/template/TemplateVisitor.java
@@ -0,0 +1,9 @@
+package xyz.bobkinn.indigoi18n.template;
+
+import xyz.bobkinn.indigoi18n.template.arg.TemplateArgument;
+
+public interface TemplateVisitor {
+    void visitPlain(String text);
+
+    void visitArgument(TemplateArgument argument);
+}
