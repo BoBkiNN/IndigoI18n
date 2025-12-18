6fcd1396bb9edd4cd636162359dfdbbff8322b9e
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-18 09:28:08 +0500

    add docs for LocaleResolver

--- core/src/main/java/xyz/bobkinn/indigoi18n/LocaleResolver.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/LocaleResolver.java
@@ -5,11 +5,20 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
 
+/**
+ * Locale resolver is used to convert language id string into instance of Locale.
+ * This is used to correctly lookup plurals rules for that language.
+ * @see #parseLocale(String) default implementation
+ */
 public interface LocaleResolver {
     Locale getLocale(String lang);
 
     LocaleResolver DEFAULT = LocaleResolver::parseLocale;
 
+    /**
+     * Default implementation of LocaleResolver.<br>
+     * It uses format {@code language_country_variant}
+     */
     static @Nullable Locale parseLocale(final @NotNull String string) {
         final String[] segments = string.split("_", 3); // language_country_variant
         final int length = segments.length;
