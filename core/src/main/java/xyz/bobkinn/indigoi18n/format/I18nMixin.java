afd3d20b880101fb84e79e78e1613562a48a68dc
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-20 20:02:20 +0500

    add doc to I18nMixin

--- core/src/main/java/xyz/bobkinn/indigoi18n/format/I18nMixin.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/format/I18nMixin.java
@@ -2,5 +2,10 @@ package xyz.bobkinn.indigoi18n.format;
 
 import xyz.bobkinn.indigoi18n.I18nBase;
 
+/**
+ * Mixins here are used to add additional methods to your I18n instance.<br>
+ * This interface only used by actual mixin interfaces.<br>
+ * @see xyz.bobkinn.indigoi18n.format.impl.StringI18nMixin I18nMixin for String format
+ */
 public interface I18nMixin extends I18nBase {
 }
