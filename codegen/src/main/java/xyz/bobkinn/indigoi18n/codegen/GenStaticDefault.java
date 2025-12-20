b68d386294d791e88f7b6574e6629f1eafdd0537
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-20 23:04:51 +0500

    add javadoc to GenStaticDefault

--- codegen/src/main/java/xyz/bobkinn/indigoi18n/codegen/GenStaticDefault.java
+++ codegen/src/main/java/xyz/bobkinn/indigoi18n/codegen/GenStaticDefault.java
@@ -2,6 +2,13 @@ package xyz.bobkinn.indigoi18n.codegen;
 
 import java.lang.annotation.*;
 
+/**
+ * Generates class named {@link #name()} in same package.<br>
+ * It contains public static final field INSTANCE with type of annotated class.
+ * This field is initialised using {@link #creator()} method.
+ * Annotation processor takes all methods from interfaces annotated class implements
+ * and generates static methods executing same method on INSTANCE.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface GenStaticDefault {
