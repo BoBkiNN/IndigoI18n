613c249f7a5a46642d3710efe532ec1dcf36b177
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-25 10:48:56 +0500

    add doc

--- core/src/main/java/xyz/bobkinn/indigoi18n/source/TranslationLoadError.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/source/TranslationLoadError.java
@@ -1,5 +1,8 @@
 package xyz.bobkinn.indigoi18n.source;
 
+/**
+ * This exception is thrown from translation loading and sources logic.
+ */
 public class TranslationLoadError extends RuntimeException {
     public TranslationLoadError(String message, Throwable cause) {
         super(message, cause);
