f9a0c317e70e69a3f28b0e3b9754e170a7319a3a
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-25 10:55:56 +0500

    add doc

--- core/src/main/java/xyz/bobkinn/indigoi18n/source/ISourceTextAdder.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/source/ISourceTextAdder.java
@@ -2,6 +2,13 @@ package xyz.bobkinn.indigoi18n.source;
 
 import xyz.bobkinn.indigoi18n.data.Translation;
 
+/**
+ * Used by sources to put texts into {@link xyz.bobkinn.indigoi18n.Translations}.
+ * Its primary goal to track what have been added
+ */
 public interface ISourceTextAdder {
+    /**
+     * This method is called by sources when they are loading translations
+     */
     void add(String key, String language, Translation text);
 }
