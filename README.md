af53ab43afdc4ac1a98bd87c5370883aa227b11f
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-12-25 10:44:26 +0500

    add DynamicResolver

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ TODO:
 - adventure support
 - bukkit support
 - kotlin bukkit extension methods
-- dynamic resolver (not uses texts stored in Translations)
 - abstraction for visitor to allow custom template parts
---
+++ core/src/main/java/xyz/bobkinn/indigoi18n/resolver/DynamicResolver.java
@@ -0,0 +1,31 @@
+package xyz.bobkinn.indigoi18n.resolver;
+
+import org.jetbrains.annotations.Nullable;
+import xyz.bobkinn.indigoi18n.Translations;
+import xyz.bobkinn.indigoi18n.context.Context;
+import xyz.bobkinn.indigoi18n.data.Translation;
+
+/**
+ * Dynamic resolver can be used to query translations bypassing {@link Translations}.<br>
+ * Its default {@link #get(Context, Translations, String, String)} method returns key if abstract method return null
+ */
+@SuppressWarnings("unused")
+public interface DynamicResolver extends TranslationResolver {
+
+    /**
+     * @return null if translation not found
+     */
+    @Nullable Translation get(Context ctx, String key, String lang);
+
+    @Override
+    default Translation get(Context ctx, Translations texts, String key, String lang) {
+        var t = get(ctx, key, lang);
+        if (t != null) return t;
+        return Translation.create(key);
+    }
+
+    @Override
+    default Translation getOrNull(Context ctx, Translations texts, String key, String lang) {
+        return get(ctx, key, lang);
+    }
+}
