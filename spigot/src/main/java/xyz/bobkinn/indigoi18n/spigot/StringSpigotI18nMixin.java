3eef85e3d50da71f0b767179e60cbdeccca9971b
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-01-19 13:50:29 +0500

    suppress unused

--- spigot/src/main/java/xyz/bobkinn/indigoi18n/spigot/StringSpigotI18nMixin.java
+++ spigot/src/main/java/xyz/bobkinn/indigoi18n/spigot/StringSpigotI18nMixin.java
@@ -7,6 +7,7 @@ import xyz.bobkinn.indigoi18n.format.impl.StringI18nMixin;
 import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("unused")
 public interface StringSpigotI18nMixin extends StringI18nMixin, CommandSenderLanguageMixin {
     default String parse(Context ctx, CommandSender viewer, String key, List<Object> args) {
         return parse(ctx, getLanguage(viewer), key, args);
