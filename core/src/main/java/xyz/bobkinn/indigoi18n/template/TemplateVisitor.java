3fa93aaef456a83501e9f1fed93e4bc0855473f7
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-01-21 14:43:05 +0500

    update TemplateVisitor docs

--- core/src/main/java/xyz/bobkinn/indigoi18n/template/TemplateVisitor.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/template/TemplateVisitor.java
@@ -2,6 +2,9 @@ package xyz.bobkinn.indigoi18n.template;
 
 import xyz.bobkinn.indigoi18n.template.arg.TemplateArgument;
 
+/**
+ * Utility class used to traverse {@link xyz.bobkinn.indigoi18n.data.ParsedEntry}
+ */
 public interface TemplateVisitor {
     void visitPlain(String text);
 
