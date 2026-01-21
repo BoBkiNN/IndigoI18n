be2396b6dba6518475ff915623503a28d0eb7037
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-01-21 15:45:53 +0500

    update AdventureFormats docs

--- adventure/src/main/java/xyz/bobkinn/indigoi18n/format/adventure/AdventureFormats.java
+++ adventure/src/main/java/xyz/bobkinn/indigoi18n/format/adventure/AdventureFormats.java
@@ -8,9 +8,20 @@ import xyz.bobkinn.indigoi18n.format.FormatType;
  */
 public class AdventureFormats {
 
+    /**
+     * Simple {@link Component#text(String)} renderer
+     */
     public static final FormatType<Component> PLAIN = new FormatType<>(Component.class, "plain");
 
+    /**
+     * Renderer that uses {@link net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer}
+     * @see xyz.bobkinn.indigoi18n.format.adventure.format.LegacyComponentI18nFormat
+     */
     public static final FormatType<Component> LEGACY = new FormatType<>(Component.class, "legacy");
 
+    /**
+     * Renderer that uses {@link net.kyori.adventure.text.minimessage.MiniMessage}
+     * @see xyz.bobkinn.indigoi18n.format.adventure.format.MiniMessageComponentI18nFormat
+     */
     public static final FormatType<Component> MINI_MESSAGE = new FormatType<>(Component.class, "minimessage");
 }
