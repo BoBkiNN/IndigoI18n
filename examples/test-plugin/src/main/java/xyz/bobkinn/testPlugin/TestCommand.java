8c4ed87642ff592e4c98cd83d3f4be3b5d31aa50
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-02-03 14:12:19 +0500

    add new plugin test

--- examples/test-plugin/src/main/java/xyz/bobkinn/testPlugin/TestCommand.java
+++ examples/test-plugin/src/main/java/xyz/bobkinn/testPlugin/TestCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import xyz.bobkinn.indigoi18n.paper.PaperAdventureI18n;
 
+import java.util.Random;
+
 @RequiredArgsConstructor
 public class TestCommand implements CommandExecutor {
     private final PaperAdventureI18n i18n;
@@ -15,13 +17,7 @@ public class TestCommand implements CommandExecutor {
         i18n.parseA(sender, "myplugin.test", 123_123.12345);
     }
 
-    @Override
-    public boolean onCommand(
-            @NotNull CommandSender sender,
-            @NotNull Command command,
-            @NotNull String label,
-            String[] args
-    ) {
+    public void test1(CommandSender sender) {
         for (int i = 0; i < 5_000; i++) {
             perform(sender);
         }
@@ -35,6 +31,55 @@ public class TestCommand implements CommandExecutor {
         perform(sender);
         var d2 = System.nanoTime() - t2;
         sender.sendMessage("Single call ns: "+d2);
+    }
+
+    public static final Random RANDOM = new Random(12312312);
+
+    private static final String letters = "abcdefghziours";
+
+    private static char randomLetter() {
+        var i = RANDOM.nextInt(letters.length());
+        return letters.charAt(i);
+    }
+
+    private static String randomKey() {
+        var l = RANDOM.nextInt(5, 15);
+        var sb = new StringBuilder(l);
+        for (int i = 0; i < l; i++) {
+            var isDot = RANDOM.nextFloat() > .9;
+            if (isDot) sb.append(".");
+            else {
+                sb.append(randomLetter());
+            }
+        }
+        return sb.toString();
+    }
+
+    public void test2(CommandSender sender) {
+        for (int i = 0; i < 5_000; i++) {
+            i18n.parseA(sender, randomKey(), sender);
+        }
+        var t = System.currentTimeMillis();
+        for (int i = 0; i < 50_000; i++) {
+            i18n.parseA(sender, randomKey(), sender);
+        }
+        var d = System.currentTimeMillis() - t;
+        sender.sendMessage("Mass d: "+d);
+    }
+
+    @Override
+    public boolean onCommand(
+            @NotNull CommandSender sender,
+            @NotNull Command command,
+            @NotNull String label,
+            String[] args
+    ) {
+        if (args.length < 1) return false;
+        var arg = args[0];
+        switch (arg) {
+            case "a": test1(sender); break;
+            case "b": test2(sender); break;
+        }
         return true;
     }
 }
