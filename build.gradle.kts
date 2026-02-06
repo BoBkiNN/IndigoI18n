2e055112efbe3b0058204fe99d783b1f58615c4e
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-02-07 03:29:57 +0500

    update deploy urls

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,9 +83,9 @@ subprojects {
                 name = "central"
                 url = uri(
                     if (!isRelease)
-                        "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                        "https://central.sonatype.com/repository/maven-snapshots/"
                     else
-                        "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                        "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
                 )
                 credentials {
                     username = System.getenv("CENTRAL_TOKEN_USER") ?: ""
