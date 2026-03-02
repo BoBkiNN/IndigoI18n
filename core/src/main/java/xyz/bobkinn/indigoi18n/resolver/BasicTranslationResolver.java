75175032611bb94490460990b7d14bb800e18e10
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-03-02 11:01:00 +0500

    add getMissingText to BasicTranslationResolver

--- core/src/main/java/xyz/bobkinn/indigoi18n/resolver/BasicTranslationResolver.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/resolver/BasicTranslationResolver.java
@@ -1,5 +1,6 @@
 package xyz.bobkinn.indigoi18n.resolver;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.bobkinn.indigoi18n.Translations;
 import xyz.bobkinn.indigoi18n.context.Context;
@@ -10,11 +11,24 @@ import xyz.bobkinn.indigoi18n.data.Translation;
  */
 public class BasicTranslationResolver implements TranslationResolver {
 
+    /**
+     * Called when no translation found in text map.
+     * Default implementation return text containing key itself
+     * @param texts text map used to lookup text
+     * @param key translation key
+     * @param lang requested language
+     * @return non-null translation
+     */
+    @SuppressWarnings("unused")
+    protected @NotNull Translation getMissingText(Translations texts, String key, String lang) {
+        return Translation.create(key);
+    }
+
     @Override
     public Translation get(Context ctx, Translations texts, String key, String lang) {
         var v = getOrNull(ctx, texts, key, lang);
         if (v != null) return v;
-        return Translation.create(key);
+        return getMissingText(texts, key, lang);
     }
 
     @Override
