70b6d89c61d350c6b9aa829c4ab128d92863323d
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2026-03-02 11:32:08 +0500

    add onParsingFailed in StringRenderer

--- core/src/main/java/xyz/bobkinn/indigoi18n/render/impl/StringRenderer.java
+++ core/src/main/java/xyz/bobkinn/indigoi18n/render/impl/StringRenderer.java
@@ -1,9 +1,11 @@
 package xyz.bobkinn.indigoi18n.render.impl;
 
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 import xyz.bobkinn.indigoi18n.context.Context;
 import xyz.bobkinn.indigoi18n.data.TemplateCache;
 import xyz.bobkinn.indigoi18n.render.Renderer;
+import xyz.bobkinn.indigoi18n.template.TemplateParseOptions;
 import xyz.bobkinn.indigoi18n.template.format.StringTemplateFormatter;
 import xyz.bobkinn.indigoi18n.template.format.TemplateFormatter;
 
@@ -26,11 +28,22 @@ public class StringRenderer extends Renderer<String> {
         return text;
     }
 
+    /**
+     * Called when {@link TemplateCache#getOrCompute(Context, String, TemplateParseOptions)} returned null.<br>
+     * Default implementation returns translation key text.
+     * @param input string failing to be parsed
+     * @see TemplateCache#getOrCompute(Context, String, TemplateParseOptions)
+     */
+    @SuppressWarnings("unused")
+    protected String onParsingFailed(@NotNull Context ctx, String input) {
+        return ctx.key();
+    }
+
     @Override
     public String replaceArguments(Context ctx, String input, List<Object> args) {
         var parsed = cache.getOrCompute(ctx, input);
         if (parsed == null) {
-            return ctx.key();
+            return onParsingFailed(ctx, input);
         }
         return templateFormatter.format(ctx, parsed, args);
     }
